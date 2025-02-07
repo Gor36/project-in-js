44d1f486c5e66e382fcd0f1bfbec8b638e9d7c43
Gor <ahper17052000@gmail.com>
2025-02-07 11:28:11 +0300

    add Toast

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("list-container");
+
 function addTask() {
   if (inputBox.value === "") {
-    alert("You must write something!");
+    new Toast({
+      text: "Поле не может быть пустым",
+      type: "default",
+      duration: 100000,
+    })._show();
+  } else if (inputBox.value.length > 75) {
+    new Toast({
+      text: "Текст не может быть длиннее 75 символов",
+      type: "default",
+      duration: 100000,
+    })._show();
   } else {
     let li = document.createElement("li");
     li.innerHTML = inputBox.value;
@@ -37,3 +48,43 @@ function showTask() {
   listContainer.innerHTML = localStorage.getItem("data");
 }
 showTask();
+
+if (!document.querySelector(".toast-container")) {
+  const container = document.createElement("div");
+  container.classList.add("toast-container");
+  document.body.append(container);
+}
+
+class Toast {
+  constructor(params) {
+    this._text = params.text || "Сообщение";
+    this._type = params.type || "default";
+    this._duration = params.duration || 3000;
+    this._create();
+  }
+
+  _create() {
+    this._el = document.createElement("div");
+    this._el.classList.add("toast", `toast_${this._type}`);
+    this._el.innerHTML = `
+      <div class="toast__header">Замечание</div>
+      <div class="toast__body">${this._text}</div>
+      <button class="toast__close" type="button"></button>
+    `;
+    document.querySelector(".toast-container").appendChild(this._el);
+
+    this._el.querySelector(".toast__close").addEventListener("click", () => {
+      this._hide();
+    });
+  }
+
+  _show() {
+    this._el.classList.add("toast_show");
+    setTimeout(() => this._hide(), this._duration);
+  }
+
+  _hide() {
+    this._el.classList.remove("toast_show");
+    setTimeout(() => this._el.remove(), 300);
+  }
+}
